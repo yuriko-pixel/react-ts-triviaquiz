5af3fd07813e9c18bb954b1a265aba780ffea80a
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2022-02-04 18:44:02 +0900

    fitst commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+import CategoryChoose from "./components/CategoryChoose";
+import DifficultyChoose from "./components/DifficultyChoose";
+import ChooseQuiz from "./components/ChooseQuiz";
+import Quiz from "./components/Quiz";
+import styled from "styled-components";
 
-function App() {
+const AppContainer = styled.div`
+  background: #888afb;
+  margin: 0;
+  font-family: "Trebuchet MS";
+  text-align: center;
+  color: white;
+`;
+
+export default function App() {
+  const [categoryChosen, setCategoryChosen]: [
+    number,
+    React.Dispatch<React.SetStateAction<number>>
+  ] = useState<number>(0);
+  const [difficultyChosen, setDifficultyChosen]: [
+    string,
+    React.Dispatch<React.SetStateAction<string>>
+  ] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <h1>Quizz App</h1>
+      {difficultyChosen.length === 0 ? (
+        <ChooseQuiz
+          categoryChosen={categoryChosen}
+          setCategoryChosen={setCategoryChosen}
+          difficultyChosen={difficultyChosen}
+          setDifficultyChosen={setDifficultyChosen}
+        />
+      ) : (
+        <Quiz
+          categoryChosen={categoryChosen}
+          difficultyChosen={difficultyChosen}
+        />
+      )}
+    </AppContainer>
   );
 }
-
-export default App;
